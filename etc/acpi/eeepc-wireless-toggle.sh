541cfaf0b62d84e00714d04cb1bc0f441851d0a8
adamm <adamm@7d34d15c-3439-4cde-99f9-468cb4432ce7>
2008-09-03 22:37:58 +0000

    * Added preliminary 901/1000/1000h support to eeepc-wireless-toggle.sh * Created new config file: /etc/default/eeepc-config * Updated eeepc sound-after-suspend hack to update /etc/default/alsa if   necessary. Only needed for EeePC 900 model
    git-svn-id: https://www.array.org/svn/eeepc-config/trunk@5 7d34d15c-3439-4cde-99f9-468cb4432ce7

--- etc/acpi/eeepc-wireless-toggle.sh
+++ etc/acpi/eeepc-wireless-toggle.sh
@@ -1,17 +1,86 @@
 #!/bin/sh
+######################################################################
+# eeepc-wireless-toggle.sh v1.2 by Adam McDaniel, (c) 2008
+#
+# Script that turns wireless on or off. Loads and unloads appropriate
+# modules for the Asus Eee PC model.
+######################################################################
 
-wlan_control=/proc/acpi/asus/wlan
+DMIDEC="/usr/sbin/dmidecode"
+MODPROBE="/sbin/modprobe"
 
-WLANSTATE=$(cat $wlan_control)
+PRODUCT=`${DMIDEC} -s system-product-name`
 
-case $WLANSTATE in
+WLAN_PROC="/proc/acpi/asus/wlan"
+WLAN_STATE=`cat ${WLAN_PROC}`
+
+
+# Load defaults
+
+if [ -e /etc/default/eeepc-config ]; then
+    . /etc/default/eeepc-config
+fi
+
+
+######################################################################
+#
+# functions
+#
+######################################################################
+
+disable_atheros_wireless () {
+	ifconfig ath0 down
+	${MODPROBE} -r ath_pci
+	echo 0 > ${WLAN_PROC}
+}
+
+
+enable_atheros_wireless () {
+	${MODPROBE} ath_pci
+	echo 1 > ${WLAN_PROC}
+}
+
+
+disable_ralink_wireless () {
+	ifconfig ra0 down
+	${MODPROBE} -v rt2860sta
+	echo 0 > ${WLAN_PROC}
+}
+
+
+enable_ralink_wireless () {
+	${MODPROBE} rt2860sta
+	echo 1 > ${WLAN_PROC}
+}
+
+
+######################################################################
+#
+# main
+#
+######################################################################
+
+# sanity check
+
+# If /proc/acpi/asus/wlan doesn't exist, something's wrong. Stop.
+
+if [ ! -e ${WLAN_PROC} ]; then
+	exit 0
+fi
+
+case ${WLAN_STATE} in
 	0)
-		modprobe ath_pci
-		echo 1 > $wlan_control
+		if [ ${PRODUCT} -le 900 ]; then
+			enable_atheros_wireless
+		else
+			enable_ralink_wireless
+		fi
 		;;
 	1)
-		ifconfig ath0 down
-		modprobe -r ath_pci
-		echo 0 > $wlan_control
+		if [ ${PRODUCT} -le 900 ]; then
+			disable_atheros_wireless
+		else
+			disable_ralink_wireless
+		fi
 		;;
 esac
