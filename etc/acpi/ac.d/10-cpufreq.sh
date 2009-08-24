5c724f6461f8197ebded94d8232cb5125f70b26b
Adam McDaniel <adam@jolicloud.org>
2009-08-24 12:02:33 -0600

    Enable sched_smt_power_savings on battery power

--- etc/acpi/ac.d/10-cpufreq.sh
+++ etc/acpi/ac.d/10-cpufreq.sh
@@ -8,4 +8,8 @@ if [ $STATE = "AC" ]; then
     for i in `find /sys/devices/system/cpu -name scaling_governor`; do
         echo "performance" > $i;
     done
+
+    if [ -e /sys/devices/system/cpu/sched_smt_power_savings ]; then
+        echo 0 > /sys/devices/system/cpu/sched_smt_power_savings;
+    fi
 fi
--- etc/acpi/battery.d/10-cpufreq.sh
+++ etc/acpi/battery.d/10-cpufreq.sh
@@ -8,4 +8,8 @@ if [ $STATE = "BATTERY" ]; then
     for i in `find /sys/devices/system/cpu -name scaling_governor`; do
         echo "ondemand" > $i;
     done
+
+    if [ -e /sys/devices/system/cpu/sched_smt_power_savings ]; then
+        echo 1 > /sys/devices/system/cpu/sched_smt_power_savings;
+    fi
 fi
