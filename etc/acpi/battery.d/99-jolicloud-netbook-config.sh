3620a8e4898778e28974c45236df24b40f1f96c5
Adam McDaniel <adam@jolicloud.org>
2009-09-09 13:17:13 +0200

    Disable Advanced Power Management on HDs on battery power
    By default, when switching to battery power, Ubuntu runs hdparm -B 128, but
    this produces incessant clicking on poorly made HDs.  Disable this for now,
    until a proper solution can be identified.

--- etc/acpi/battery.d/99-jolicloud-netbook-config.sh
+++ etc/acpi/battery.d/99-jolicloud-netbook-config.sh
@@ -12,4 +12,12 @@ if [ $STATE = "BATTERY" ]; then
     if [ -e /sys/devices/system/cpu/sched_smt_power_savings ]; then
         echo 1 > /sys/devices/system/cpu/sched_smt_power_savings;
     fi
+
+    # Disable Advanced Power Management on all drives. This disables the
+    # incessant clicking heard by cheap harddrives while on battery power.
+    for d in `ls /dev/disk/by-id | grep scsi | grep -v "\-part"`; do
+        /sbin/hdparm -B 255 /dev/disk/by-id/$d > /dev/null
+    done
 fi
+
+return 0
